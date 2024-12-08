1a432cbbdb1a9e6d5fcd5beb7895fd2c651dea5c
Ivan Vorvanin <virtalhipriv@gmail.com>
2024-12-08 21:25:04 +0500

    idk

---
+++ pseudo_due_os/config.h
@@ -0,0 +1,22 @@
+// pins configuration
+
+// Video configuraton 
+#define USE_NTSC
+
+// Build configuration
+#define ENABLE_SERIAL
+#define LOG_OUTPUT_TO_SERIAL
+
+// Cursor settings
+#define cursorBlinkDelay 30
+
+// Keyboard keys config
+#define OEM_ENTER 40
+#define OEM_BACKSPACE 42
+
+// Keybuffer configuration
+#define KEYBUFFER_SIZE 256
+#undef KEYPOINTER_SUPPORT_LARGER_DATA_TYPE // you can enable it, but its necessary
+
+// Lastcmd size
+#define LASTCMD_SIZE 256 // W.I.P. function
--- pseudo_due_os/pseudo_due_os.ino
+++ pseudo_due_os/pseudo_due_os.ino
@@ -4,34 +4,27 @@
 #include <KeyboardController.h>
 #include <VGA.h>
 #include <complex>
-// hardware configuration
-#define RESET 2
-// build configuration
-#define DEBUG
 
-// keyboard keys config
-#define OEM_ENTER 40
-#define OEM_BACKSPACE 42
-
-// keybuffer size
-#define KEYBUFFER_SIZE 256
-// lastcmd size
-#define LASTCMD_SIZE 256 
+#include "config.h"
 
 USBHost usb;
 KeyboardController keyboard(usb);
 
 bool KEY_ENTER_LOCKED;
+bool NEW_LINE;
 
 // keybuffer
 char keybuffer[KEYBUFFER_SIZE];
 char keygoreargbuffer[KEYBUFFER_SIZE];
-
-// last command buffer
-
-// change to a larger data type (uint16_t, unsigned int, etc.) if you want to expand it. the maximum uint8_t can hold is 256
+#ifdef KEYPOINTER_SUPPORT_LARGER_DATA_TYPE
+uint16_t keypointer;
+#else
 uint8_t keypointer;
-uint16_t c;
+#endif
+
+// inital position
+byte Ycursor = 3;
+byte Xcursor = 2;
 
 // 1. cpu is halting if i use this 
 // extern "C" char* sbrk(int incr);
@@ -43,9 +36,12 @@ uint16_t c;
 
 void keyPressed() {
   char key = keyboard.getKey();
-  #ifdef DEBUG
+  #ifdef LOG_OUTPUT_TO_SERIAL
   Serial.print("Pressed:");
   #endif
+  if (NEW_LINE == 1){
+  VGA.print("> ");
+  }
   uint8_t oemCode = keyboard.getOemKey();
   printKey(key, oemCode, keyboard.getModifiers());
   if (oemCode == OEM_ENTER) { /* enter */
@@ -54,7 +50,7 @@ void keyPressed() {
     VGA.write('\n');
     commandProcessor();
     if (keypointer != 0) {
-      #ifdef DEBUG
+      #ifdef LOG_OUTPUT_TO_SERIAL
       Serial.print("resetting the buffer. was: ");
       Serial.println(keybuffer);
       #endif
@@ -62,19 +58,28 @@ void keyPressed() {
       // needs to be rewritten
 //    char lastcmd[LASTCMD_SIZE] = keybuffer;
       keybuffer[0] = '\0';
+      NEW_LINE = 1;
       }
+      NEW_LINE = 0;
+      VGA.print("> ");
+      Xcursor = 2;
     }
     return;
   }
+  // bad backspace implementation
   if (oemCode == OEM_BACKSPACE) {
-    //VGA.write('\b');
+    Xcursor = Xcursor - 2;
+    VGA.moveCursor(Xcursor, Ycursor);
+    VGA.print(" ");
+    Xcursor = Xcursor - 1;
+    Ycursor = Ycursor - 2;
     if (keypointer != 0) {
       keybuffer[--keypointer] = '\0';
     }
     return;
   }
   if (keypointer >= KEYBUFFER_SIZE-1) {
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.println("the keybuffer be over. resetting");
     #endif
     keybuffer[0] = '\0';
@@ -83,13 +88,13 @@ void keyPressed() {
   if (KEY_ENTER_LOCKED == 1){ // buffer reset disabled
     keygoreargbuffer[keypointer] = key;
     keygoreargbuffer[++keypointer] = '\0';
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.println(keygoreargbuffer);
     #endif
   } else {
     keybuffer[keypointer] = key;
     keybuffer[++keypointer] = '\0';
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.println(keybuffer);
     #endif
   }
@@ -101,7 +106,7 @@ void keyPressed() {
 //}
 
 void printKey(char key, uint8_t oemCode, uint8_t mod) {
-  #ifdef DEBUG
+  #ifdef LOG_OUTPUT_TO_SERIAL
   Serial.print(" key:");
   Serial.print(oemCode);
   Serial.print(" mod:");
@@ -110,61 +115,62 @@ void printKey(char key, uint8_t oemCode, uint8_t mod) {
   #endif
   
   if (mod & LeftCtrl) {
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.print("L-Ctrl ");
     #endif
   }
   if (mod & LeftShift) {
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.print("L-Shift ");
     #endif
   }
   if (mod & Alt) {
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.print("Alt ");
     #endif
   }
   if (mod & LeftCmd) {
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.print("L-Cmd ");
     #endif
   }
   if (mod & RightCtrl) {
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.print("R-Ctrl ");
     #endif
   }
   if (mod & RightShift) {
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.print("R-Shift ");
     #endif
   }
   if (mod & AltGr) {
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.print("AltGr ");
     #endif
   }
   if (mod & RightCmd) {
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.print("R-Cmd ");
     #endif
   } 
 
   if (oemCode == OEM_ENTER) {
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.println("Enter was pressed");
     #endif
     return;
 };
 
   if (oemCode == OEM_BACKSPACE) {
-    #ifdef DEBUG
+    #ifdef LOG_OUTPUT_TO_SERIAL
     Serial.println("backspace");
     #endif
     return;
   };
 
  // using VGA.print instead of VGA.write
+  Xcursor = Xcursor + 1;
   VGA.print(key);
   Serial.println(key);
 }
@@ -172,15 +178,16 @@ void printKey(char key, uint8_t oemCode, uint8_t mod) {
 void setup() {
   keybuffer[0] = '\0';
   keypointer = 0;
-  pinMode(RESET, OUTPUT);
-  digitalWrite(RESET, HIGH);
-  #ifdef DEBUG
+  #ifdef ENABLE_SERIAL
   Serial.begin(9600);
+  #endif
+  #ifdef LOG_OUTPUT_TO_SERIAL
   Serial.print("Program started\n");
   #endif
   VGA.beginPAL();
   VGA.print("p-OS\n");
   delay(200);
+  VGA.print("> ");
 }
 
 // bool ps2test_started = 0; unused code
@@ -189,16 +196,15 @@ bool newton_started = 0;
 void loop() {
   // Process USB tasks
   usb.Task();
+//cursorBlink();
 }
 
 void reset() {
-      #ifdef DEBUG
+      #ifdef LOG_OUTPUT_TO_SERIAL
       Serial.println("RESETTING");
       #endif
-      digitalWrite(RESET, LOW);
-      VGA.println("If you see this text, you didn't connect reset");
+      setup(); // do not use reset, system will crash
 }
-
 void commandProcessor() {
   if(strcmp(keybuffer , ".") != 0) { // it ignoring prefix, needs to fix
    if (strcmp(keybuffer, ".circle") == 0) {
@@ -215,6 +221,14 @@ void commandProcessor() {
   //    VGA.println(freeRam());
    } else if (strcmp(keybuffer, ".elt") == 0) {
      #include "programs\enter_lock_test.h"
+  //  } else if (strcmp(keybuffer, ".cleargfx") == 0) {
+  //   VGA.clear();
+   } else if (strcmp(keybuffer, ".curtest") == 0) {
+    // cursor position test
+    VGA.print("X: ");
+    VGA.print(Xcursor);
+    VGA.println("Y:");
+    VGA.print(Ycursor);
    } else {
      VGA.print("Command not found\n");
      return;
@@ -224,3 +238,20 @@ void commandProcessor() {
   keybuffer[0] = '\0';
   keypointer = 0;
 }
+
+// void cursorGet(char XY) {
+//   if (XY == "X") {
+
+//    return curX
+//   }
+// }
+
+// don't use this, it doesn't work
+
+// void cursorBlink() {
+//   delay(cursorBlinkDelay);
+//   VGA.print("_");
+//   Xcursor = Xcursor - 1;
+//   VGA.moveCursor(Xcursor, Ycursor);
+//   VGA.print(" ");
+// }
