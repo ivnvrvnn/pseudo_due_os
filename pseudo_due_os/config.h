0b61d48255b043fde451ad3ab4d4720c7f670c82
Ivan Vorvanin <virtalhipriv@gmail.com>
2024-12-12 20:27:34 +0500

    Default config

--- pseudo_due_os/config.h
+++ pseudo_due_os/config.h
@@ -4,8 +4,8 @@
 #undef USE_NTSC
 
 // Build configuration
-#define ENABLE_SERIAL
-#define LOG_OUTPUT_TO_SERIAL
+#undef ENABLE_SERIAL
+#undef LOG_OUTPUT_TO_SERIAL
 
 // Cursor settings
 #define cursorBlinkDelay 30
@@ -18,5 +18,3 @@
 #define KEYBUFFER_SIZE 256
 #undef KEYPOINTER_SUPPORT_LARGER_DATA_TYPE // you can enable it, but its necessary
 
-// Lastcmd size
-#define LASTCMD_SIZE 256 // W.I.P. function
